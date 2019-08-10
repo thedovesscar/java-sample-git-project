fa087b79b8cefadc4bbf59d880fed9de7d790d08
Gurmehar <thedovesscar@gmail.com>
2019-08-10 01:06:11 -0700

    Updated UserDemo class to test both User constructors

--- src/UserDemo.java
+++ src/UserDemo.java
@@ -4,11 +4,14 @@ public class UserDemo {
 	public static void main(String[] args) {
 		
 		User person = new User();
+		User person2 = new User("Felicia", 23);
 		
 		person.setAge(44);
 		person.setName("Johnny Boy");
 		
+		
 		System.out.println(person);
-		System.out.println("Wow Cool Job!");
+		
+		System.out.println(person2);
 	}
 }
