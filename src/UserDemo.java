4c1a710e813dcc697d51dc8e9686b3b983f64852
Gurmehar <thedovesscar@gmail.com>
2019-08-10 00:50:29 -0700

    Added code to UserDemo class to test User class

--- src/UserDemo.java
+++ src/UserDemo.java
@@ -3,5 +3,11 @@ public class UserDemo {
 	
 	public static void main(String[] args) {
 		
+		User person = new User();
+		
+		person.setAge(44);
+		person.setName("Johnny Boy");
+		
+		System.out.println(person);
 	}
 }
