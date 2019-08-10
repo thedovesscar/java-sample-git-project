9e1400bceec2996f690e1dd0c37ae4bacdbf1846
Gurmehar <thedovesscar@gmail.com>
2019-08-09 19:21:33 -0700

    Added getter and setter for name field

--- src/User.java
+++ src/User.java
@@ -3,4 +3,14 @@ public class User {
 
 	private String name;
 	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
